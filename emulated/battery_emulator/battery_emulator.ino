6f028bdab1ef5c6c36fa189c587629409f0e237b
STPFox1165 <mariafernado@gmail.com>
2025-10-19 23:27:26 +0100

    Add files via upload

---
+++ emulated/battery_emulator/battery_emulator.ino
@@ -0,0 +1,52 @@
+#define NET_ID 01 //network ID, change as needed !!MAKE SURE NO TWO DEVICES HAVE THE SAME!!
+uint16_t volt_raw, cur_raw; //raw values of the readings
+float volt, cur; //final values
+byte RGB[3] = {10, 9, 11}; //rgb pins (cause they're there)
+byte serial_mem[5]; //serial coms memeory, "standard" for the project
+// ID, command/data, 3x data
+void setup() {
+  Serial.begin(9600); //start UART communication (usb too)
+
+  //these arent used yet
+  pinMode(RGB[0], OUTPUT);
+  pinMode(RGB[1], OUTPUT);
+  pinMode(RGB[2], OUTPUT);
+}
+
+void loop() {
+  do {
+    //read the ADC on the respective pins
+    volt_raw = analogRead(A0);
+    cur_raw = analogRead(A1);
+
+    //current conversion, takes a bit more effort because is -50 to +0
+    if (cur_raw > 523) {
+      cur = cur_raw - 523;
+      cur = 0 - (cur / 10.0);
+    }
+    else if (cur_raw < 500) {
+      cur = 500 - cur_raw;
+      cur = cur / 10.0;
+    }
+    else cur = 0;
+//voltage is more straight forward...
+    volt = 16.0 - ((volt_raw / 1023.0) * 8.0);
+    delay(5);
+  } while (Serial.available() < 5);
+  //read the buffer
+  for (byte i = 0; i < 5; i++) {
+    serial_mem[i] = Serial.read();
+  }
+  Serial.flush(); //empty the buffer. 
+  //in case there was a lost byte this just empties the buffer at the cost of two packets (not a big deal)
+  if (serial_mem[0] = NET_ID) {
+    switch (serial_mem[1]) {
+      case 1:
+        Serial.print(cur);
+        break;
+      case 2:
+        Serial.print(volt);
+        break;
+    }
+  }
+}
